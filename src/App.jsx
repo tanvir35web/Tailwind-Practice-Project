06c2e237097fdd356f9f5d04904cd6147861fcec
tanvir35web <islammdtanvirul@gmail.com>
2023-12-27 16:38:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+<div className="flex flex-wrap">
+<h1 className="text-2xl font-thin font-sans w-40 h-40 bg-slate-300 rounded-xl p-20 m-auto my-20 ">
+        Hello world!
+      </h1>
+      <h1 className="text-2xl font-thin font-sans w-40 h-40 bg-slate-500 rounded-xl p-20 m-auto my-20 ">
+        Hello world!
+      </h1>
+      <h1 className="text-2xl font-thin text-white font-sans w-40 h-40 bg-slate-700 rounded-xl p-20 m-auto my-20 ">
+        Hello world!
+      </h1>
+      <h1 className="text-2xl font-thin font-sans w-40 h-40 bg-stone-500 rounded-xl p-20 m-auto my-20 ">
+        Hello world!
+      </h1>
+      <h1 className="text-2xl font-thin font-sans w-40 h-40 bg-orange-500 hover:bg-green-400 hover:w-60 transition-all rounded-xl p-20 m-auto my-20 ">
+        Hello world!
+      </h1>
+</div>
+
+<div className="bg-purple-950 w-96 rounded-2xl m-auto hover:bg-teal-400 hover:rounded-sm transition-all shadow-xl font-bold">
+  <p className="text-white text-4xl text-center p-10">Hello Bangladesh</p>
+</div>
+
+<div className="bg-gradient-to-r from-cyan-500 to-blue-500 w-96 m-auto rounded-3xl my-5">
+    <p className="text-7xl text-center p-10 bg-origin-padding">GRADIENT</p>
+</div>
+<div className=" w-full bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500 text-7xl font-semibold">Hello Gradient</div>
+<div className="bg-[url('https://scontent.fcla2-1.fna.fbcdn.net/v/t39.30808-6/329510532_912277063444757_2477866140692401560_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=783fdb&_nc_eui2=AeEdGnQi308dln0jeqqF0FmlPmkndSIsegQ-aSd1Iix6BCh_q39VJ6lLLNpY0pt4PiFfZ8O4pgqcT3GW-tXawf4T&_nc_ohc=_Iv6jU-lmq0AX_Rb1nr&_nc_zt=23&_nc_ht=scontent.fcla2-1.fna&oh=00_AfBTkEcYMBp2pL-peh4PNXkWXAaykLrIt5Z_9-3I_DbAFQ&oe=6591C6C1')] bg-center bg-cover bg-no-repeat h-screen text-center text-8xl " > <span className="align-middle font-bold">FRIENDS</span> </div>
+
+
+    </>
+  );
+}
+
+export default App;
